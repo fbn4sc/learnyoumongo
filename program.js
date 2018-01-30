9129467fa1f327eeb9dd77bc5df157ed50f3d2af
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-29 22:06:06 -0200

    Finds documents using a query

---
+++ program.js
@@ -0,0 +1,16 @@
+const mongo = require("mongodb").MongoClient;
+
+mongo.connect("mongodb://localhost:27017/learnyoumongo", (err, client) => {
+  if (err) throw err;
+
+  const db = client.db("learnyoumongo");
+
+  db
+    .collection("parrots")
+    .find({ age: { $gt: parseInt(process.argv[2], 10) } })
+    .toArray((err, docs) => {
+      console.log(docs);
+    });
+
+  client.close();
+});
