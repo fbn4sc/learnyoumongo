f36d34f1b648885c52f32f4c4b927ea630395778
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-29 22:31:45 -0200

    Counts the documents that meet the criteria

--- program.js
+++ program.js
@@ -3,11 +3,15 @@ const mongo = require("mongodb").MongoClient;
 mongo.connect("mongodb://localhost:27017/learnyoumongo", (err, client) => {
   if (err) throw err;
 
-  const db = client.db(process.argv[2]);
+  const db = client.db("learnyoumongo");
 
-  db.collection(process.argv[3]).remove({ _id: process.argv[4] }, err => {
-    if (err) throw err;
+  db
+    .collection("parrots")
+    .count({ age: { $gt: +process.argv[2] } }, (err, count) => {
+      if (err) throw err;
 
-    client.close();
-  });
+      console.log(count);
+
+      client.close();
+    });
 });
