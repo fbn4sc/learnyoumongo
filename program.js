5d40e404cbce62bfd6ec3c371711727ab660987d
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-29 22:15:42 -0200

    Specifies the fields to be returned

--- program.js
+++ program.js
@@ -8,6 +8,7 @@ mongo.connect("mongodb://localhost:27017/learnyoumongo", (err, client) => {
   db
     .collection("parrots")
     .find({ age: { $gt: parseInt(process.argv[2], 10) } })
+    .project({ name: true, age: true, _id: false })
     .toArray((err, docs) => {
       console.log(docs);
     });
