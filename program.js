b25cfc85e946b52e5ca3f25c5951fd4e299e4f98
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-29 22:42:10 -0200

    Calculates an average using an aggregate

--- program.js
+++ program.js
@@ -6,11 +6,15 @@ mongo.connect("mongodb://localhost:27017/learnyoumongo", (err, client) => {
   const db = client.db("learnyoumongo");
 
   db
-    .collection("parrots")
-    .count({ age: { $gt: +process.argv[2] } }, (err, count) => {
+    .collection("prices")
+    .aggregate([
+      { $match: { size: process.argv[2] } },
+      { $group: { _id: "avg", avg: { $avg: "$price" } } }
+    ])
+    .toArray((err, results) => {
       if (err) throw err;
 
-      console.log(count);
+      console.log(Number(results[0].avg).toFixed(2));
 
       client.close();
     });
