fdb422dddb08cf5d4c716a2c7113a149aae576de
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-29 22:24:00 -0200

    Updates a document

--- program.js
+++ program.js
@@ -3,15 +3,11 @@ const mongo = require("mongodb").MongoClient;
 mongo.connect("mongodb://localhost:27017/learnyoumongo", (err, client) => {
   if (err) throw err;
 
-  const db = client.db("learnyoumongo");
+  const db = client.db(process.argv[2]);
 
-  const myDoc = { firstName: process.argv[2], lastName: process.argv[3] };
-
-  db.collection("parrots").insert(myDoc, (err, data) => {
-    if (err) throw err;
-
-    console.log(JSON.stringify(myDoc));
-  });
+  db
+    .collection("users")
+    .update({ username: "tinatime" }, { $set: { age: 40 } });
 
   client.close();
 });
